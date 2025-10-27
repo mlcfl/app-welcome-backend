61b5f32f0e52a5fd1fdd5ed225fdf0ed0a7d2405
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2025-10-28 00:50:55 +0300

    feat(server-port): support cli, env and default value

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
+import { getServerPort } from "@shared/backend";
 import { server } from "./server";
 
 import "dotenv/config";
 
-// Parse command line arguments
-const args = process.argv.slice(2);
-const portArg = args.find((arg) => arg.startsWith("--port="));
-const port = portArg
-	? parseInt(portArg.split("=")[1])
-	: process.env.PORT || 7200;
-
+const port = getServerPort();
 const instance = await server();
 
 instance.listen(port, () => {
