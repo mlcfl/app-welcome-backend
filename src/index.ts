6a8e73b3227c63f1f4e0798d3a15a579c5bf0be4
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2025-11-28 23:27:40 +0100

    build: update .env usage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { getServerPort, onServerStarted } from "@shared/backend";
 import { server } from "./server";
 
-import "dotenv/config";
-
 const port = getServerPort();
 const instance = await server();
 
