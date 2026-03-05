177ffcea9a4c14854f1bd3614374b6942e1d25f4
FLighter <21360067+FLighter7@users.noreply.github.com>
2026-03-05 18:39:10 +0100

    refactor: remove useless, update errorHandler

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,28 @@
 import { resolve } from "node:path";
 import { NestFactory } from "@nestjs/core";
 import type { NestExpressApplication } from "@nestjs/platform-express";
-import type { Request, Response, NextFunction } from "express";
-import { getAppName, initHTMLPagesRender } from "@shared/backend";
+import {
+	expressErrorHandler,
+	getAppName,
+	initHTMLPagesRender,
+} from "@shared/backend";
 import packageJson from "../package.json" assert { type: "json" };
 import { AppModule } from "./app.module";
-
-const errorHandler = (
-	error: unknown,
-	req: Request,
-	res: Response,
-	next: NextFunction
-): void => {
-	if (res.headersSent) {
-		return;
-	}
-
-	console.error(error);
-	res.status(500).send("Internal server error");
-};
 
 export const server = async () => {
 	const appName = getAppName(packageJson);
 	const frontendRoot = resolve(
 		import.meta.dirname,
-		`../../${appName}-frontend`
+		`../../${appName}-frontend`,
 	);
 
 	const nestApp = await NestFactory.create<NestExpressApplication>(AppModule);
 	const expressApp = nestApp.getHttpAdapter().getInstance();
 
 	await initHTMLPagesRender(expressApp, frontendRoot);
-
 	await nestApp.init();
 
-	expressApp.use(errorHandler);
+	expressApp.use(expressErrorHandler);
 
 	return expressApp;
 };
